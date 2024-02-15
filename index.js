5f8e9a29801dd27ef3e85f363f0969c365b8354c
ani_ghost <57623270+anighost1@users.noreply.github.com>
2024-02-15 19:18:35 +0530

    cors origins added

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.use(express.json())
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'http://127.0.0.1:3000'
+        'http://127.0.0.1:3000',
+        'https://paradmin.tigga.in/',
+        'https://paradmin.onrender.com/'
     ],
     credentials: true,
 }))
