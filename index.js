ae6a3b40117f09048493a67e3f6d30e4e67e05ce
ani_ghost <57623270+anighost1@users.noreply.github.com>
2024-02-17 13:06:47 +0530

    header middleware updated

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Private-Network', 'true');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
