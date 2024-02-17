da495322f329a0101365611866e68132fa14cef5
ani_ghost <57623270+anighost1@users.noreply.github.com>
2024-02-17 13:47:51 +0530

    set trust proxy

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
-
+app.set('trust proxy', 1);
 app.use(cookieParser())
 const port = process.env.PORT || 6969
 
